fef73f5bf32ac772889522430fc2ed3097643f1b
Administrator <Administrator@WIN-30MDGHVJCOH>
2020-09-04 14:54:50 +0530

    Pdf Download

--- src/main/java/com/as/DocGenegatr/DocController.java
+++ src/main/java/com/as/DocGenegatr/DocController.java
@@ -1,26 +1,41 @@
 package com.as.DocGenegatr;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
 
 @RestController
 public class DocController {
@@ -30,7 +45,6 @@ public class DocController {
 
 	@GetMapping("getDoc")
 	public void dashboard(HttpServletResponse response) {
-
 		try {
 			XWPFDocument doc = new XWPFDocument(OPCPackage
 					.open(ResourceUtils.getFile(env.getProperty("gujrati.document.filePath"))));
@@ -72,10 +86,8 @@ public class DocController {
 				}
 			}
 //			doc.write(new FileOutputStream(new File("c://createdocument.docx")));
-			
 			File file = new File("createdocument.docx");
 	        doc.write(new FileOutputStream(file));
-	        
 	        if (file.exists()) {
 	            String mimeType = URLConnection.guessContentTypeFromName(file.getName());
 	            if (mimeType == null) {
@@ -85,10 +97,39 @@ public class DocController {
 	            response.setContentLength((int) file.length());
 	            InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
 	            FileCopyUtils.copy(inputStream, response.getOutputStream());
-
 	        }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@GetMapping("getPdf")
+	public void exportToPDF(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+		try {
+			Data data = new Data("8596582", "Sahil Patil", "03/8/2020", "$2,000.00", "1005205", "452562");
+			StringTemplate page = getStringTemplate();
+			page.setAttribute("data", data);
+			String content = page.toString();
+			final HtmlCleaner htmlCleaner = new HtmlCleaner();
+			final TagNode tagNode = htmlCleaner.clean(content);
+			content = htmlCleaner.getInnerHtml(tagNode);
+			Document document = new Document();
+			response.setContentType("application/pdf");
+			PdfWriter writer = PdfWriter.getInstance(document, response.getOutputStream());
+			StringBuilder htmlString = new StringBuilder();
+			htmlString.append(content);
+			document.open();
+			InputStream is = new ByteArrayInputStream(htmlString.toString().getBytes("utf-8"));
+			Reader targetReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+			XMLWorkerHelper.getInstance().parseXHtml(writer, document, targetReader);
+			document.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	StringTemplate getStringTemplate() {
+		final StringTemplateGroup group = new StringTemplateGroup("Generators");
+		return group.getInstanceOf("employee_details");
 	}
 }
