eec5a90a6cf5f20196ea177a13fbf7b15a199fea
Sahil Patil <sahil4683@gmail.com>
2020-09-15 00:02:04 +0530

    Bonafide and lc,trial download

---
+++ src/main/java/com/as/controller/school2/School2_BonafideController.java
@@ -0,0 +1,115 @@
+package com.as.controller.school2;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.as.entity.school2.School2_StudentEntity;
+import com.as.repository.school2.School2_StudentRepository;
+
+@Controller
+public class School2_BonafideController {
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	School2_StudentRepository repository;
+
+	@GetMapping("/school2/bonafide")
+	public ModelAndView attendance() {
+		ModelAndView modelAndView = new ModelAndView("school2/bonafide");
+		modelAndView.addObject("pageTitle", "Get Bonafide");
+		return modelAndView;
+	}
+
+	@GetMapping("/school2/bonafide/{registerNumber}")
+	public void dashboard(@PathVariable(value = "registerNumber") String registerNumber, HttpServletResponse response) {
+		School2_StudentEntity student = repository.findByRegisterNumber(registerNumber);
+		try {
+			XWPFDocument doc = new XWPFDocument(OPCPackage
+					.open(ResourceUtils.getFile(env.getProperty("school2.bonafide_certificate.doc.filePath"))));
+			LocalDate dt = LocalDate.now();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yy");
+			for (XWPFParagraph p : doc.getParagraphs()) {
+				for (XWPFRun r : p.getRuns()) {
+					String text = r.getText(0);
+					if (text != null && text.contains("$date$")) {
+						text = text.replace("$date$", formatter.format(dt));
+						r.setText(text, 0);
+					}
+					if (text != null && text.contains("$name$")) {
+						text = text.replace("$name$",
+								student.getFirstName() + " " + student.getMiddleName() + " " + student.getLastName());
+						r.setText(text, 0);
+					}
+					if (text != null && text.contains("$standard$")) {
+						text = text.replace("$standard$", student.getClassIn());
+						r.setText(text, 0);
+					}
+					if (text != null && text.contains("$year$")) {
+						text = text.replace("$year$", "2020");
+						r.setText(text, 0);
+					}
+					if (text != null && text.contains("$dob$")) {
+						SimpleDateFormat fromFormat = new SimpleDateFormat("yyyy-MM-dd");
+						SimpleDateFormat toFormat = new SimpleDateFormat("dd-MM-yyyy");
+						String reformattedStr = toFormat.format(fromFormat.parse(student.getDateOfBirth()));
+						text = text.replace("$dob$", reformattedStr);
+						r.setText(text, 0);
+					}
+					if (text != null && text.contains("$cast$")) {
+						text = text.replace("$cast$", student.getCaste());
+						r.setText(text, 0);
+					}
+					if (text != null && text.contains("$reg$")) {
+						text = text.replace("$reg$", student.getRegisterNumber());
+						r.setText(text, 0);
+					}
+					r.setText(text, 0);
+				}
+			}
+//			doc.write(new FileOutputStream(new File("c://createdocument.docx")));
+			File file = new File("BonafideSc2Temp.docx");
+			doc.write(new FileOutputStream(file));
+			if (file.exists()) {
+				String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+				if (mimeType == null) {
+					mimeType = "application/octet-stream";
+				}
+				response.setContentType(mimeType);
+				response.setContentLength((int) file.length());
+				response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+						"attachment; filename=\"" + registerNumber + "_" + new Date().toString() + ".docx" + "\"");
+				InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+				FileCopyUtils.copy(inputStream, response.getOutputStream());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
