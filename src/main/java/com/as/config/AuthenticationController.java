29c3c4eb2bad57fabe8fb799d2620ec43c5795ea
Sahil Patil <sahil4683@gmail.com>
2020-09-15 15:32:19 +0530

    student count on dashboard

--- src/main/java/com/as/config/AuthenticationController.java
+++ src/main/java/com/as/config/AuthenticationController.java
@@ -3,9 +3,12 @@ package com.as.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.as.repository.school1.School1_StudentRepository;
+import com.as.repository.school2.School2_StudentRepository;
+import com.as.repository.school3.School3_StudentRepository;
 
 @RestController
 public class AuthenticationController {
@@ -13,10 +16,20 @@ public class AuthenticationController {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	School1_StudentRepository sc1;
+	@Autowired
+	School3_StudentRepository sc3;
+	@Autowired
+	School2_StudentRepository sc2;
+	
 	@GetMapping("")
 	public ModelAndView dashboard() {
 		ModelAndView modelAndView = new ModelAndView("dashboard");
-		modelAndView.addObject("pageTitle", "Home"); 
+		modelAndView.addObject("pageTitle", "Home");
+		modelAndView.addObject("school1", sc1.count());
+		modelAndView.addObject("school2", sc2.count());
+		modelAndView.addObject("school3", sc3.count());
 		return modelAndView;
 	}
 	
