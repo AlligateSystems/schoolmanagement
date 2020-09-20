a014fa6985d2c1d9138ad8b61672febd5ba4921f
Sahil Patil <sahil4683@gmail.com>
2020-09-20 16:06:54 +0530

    result validation and school 3 get result change

--- src/main/java/com/as/controller/school1/School1_ResultController.java
+++ src/main/java/com/as/controller/school1/School1_ResultController.java
@@ -23,7 +23,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.as.config.BaseResponse;
+import com.as.config.ResponseType;
 import com.as.entity.school1.School1_MarksEntity;
 import com.as.entity.school1.School1_StudentEntity;
 import com.as.repository.school1.School1_MarksRepository;
@@ -46,286 +50,29 @@ public class School1_ResultController {
 	@Autowired
 	School1_MarksRepository marksRepository;
 
-//	@GetMapping("/school1/result/{registerNumber}")
-//	public void getBonafideCertificate(@PathVariable(value = "registerNumber") String registerNumber,
-//			HttpServletResponse response, HttpServletRequest request)
-//			throws IOException, DocumentException, ParseException {
-//
-//		School1_StudentEntity student = studentRepository.findByRegisterNumber(registerNumber);
-//		if (student != null) {
-//
-//			List<School1_MarksEntity> marksList = marksRepository.findByRegisterNumber(registerNumber);
-//
-//			try {
-//				ClassPathResource cpr = new ClassPathResource(env.getProperty("school1.result.doc.filePath"));
-//				
-//				XWPFDocument doc = new XWPFDocument(cpr.getInputStream());
-//				/*
-//				 * For Table
-//				 */
-//				for (XWPFTable tbl : doc.getTables()) {
-//					for (XWPFTableRow row : tbl.getRows()) {
-//						for (XWPFTableCell cell : row.getTableCells()) {
-//							for (XWPFParagraph p : cell.getParagraphs()) {
-//								for (XWPFRun r : p.getRuns()) {
-//									String text = r.getText(0);
-//									/*
-//									 * For Marks Table
-//									 */
-//									for (School1_MarksEntity mark : marksList) {
-//										if (mark.getSubject().equalsIgnoreCase("Gujarati")) {
-//											if (text != null && text.contains("|a1|")) {
-//												text = text.replace("|a1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|a2|")) {
-//												text = text.replace("|a2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|a3|")) {
-//												text = text.replace("|a3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|a4|")) {
-//												text = text.replace("|a4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("Mathematics")) {
-//											if (text != null && text.contains("|b1|")) {
-//												text = text.replace("|b1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|b2|")) {
-//												text = text.replace("|b2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|b3|")) {
-//												text = text.replace("|b3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|b4|")) {
-//												text = text.replace("|b4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("ScienceTechnology")) {
-//											if (text != null && text.contains("|c1|")) {
-//												text = text.replace("|c1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|c2|")) {
-//												text = text.replace("|c2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|c3|")) {
-//												text = text.replace("|c3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|c4|")) {
-//												text = text.replace("|c4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("SocialScience")) {
-//											if (text != null && text.contains("|d1|")) {
-//												text = text.replace("|d1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|d2|")) {
-//												text = text.replace("|d2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|d3|")) {
-//												text = text.replace("|d3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|d4|")) {
-//												text = text.replace("|d4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("Hindi")) {
-//											if (text != null && text.contains("|e1|")) {
-//												text = text.replace("|e1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|e2|")) {
-//												text = text.replace("|e2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|e3|")) {
-//												text = text.replace("|e3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|e4|")) {
-//												text = text.replace("|e4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("English")) {
-//											if (text != null && text.contains("|f1|")) {
-//												text = text.replace("|f1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|f2|")) {
-//												text = text.replace("|f2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|f3|")) {
-//												text = text.replace("|f3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|f4|")) {
-//												text = text.replace("|f4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("Sanskrit")) {
-//											if (text != null && text.contains("|g1|")) {
-//												text = text.replace("|g1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|g2|")) {
-//												text = text.replace("|g2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|g3|")) {
-//												text = text.replace("|g3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|g4|")) {
-//												text = text.replace("|g4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//
-//										if (mark.getSubject().equalsIgnoreCase("Industry")) {
-//											if (text != null && text.contains("|h1|")) {
-//												text = text.replace("|h1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|h2|")) {
-//												text = text.replace("|h2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|h3|")) {
-//												text = text.replace("|h3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|h4|")) {
-//												text = text.replace("|h4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("Computer")) {
-//											if (text != null && text.contains("|i1|")) {
-//												text = text.replace("|i1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|i2|")) {
-//												text = text.replace("|i2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|i3|")) {
-//												text = text.replace("|i3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|i4|")) {
-//												text = text.replace("|i4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("Picture")) {
-//											if (text != null && text.contains("|j1|")) {
-//												text = text.replace("|j1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|j2|")) {
-//												text = text.replace("|j2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|j3|")) {
-//												text = text.replace("|j3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|j4|")) {
-//												text = text.replace("|j4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("PT")) {
-//											if (text != null && text.contains("|k1|")) {
-//												text = text.replace("|k1|", mark.getTotal());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|k2|")) {
-//												text = text.replace("|k2|", mark.getObtainedMarks());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|k3|")) {
-//												text = text.replace("|k3|", mark.getObtainedGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|k4|")) {
-//												text = text.replace("|k4|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//
-//									}
-//								}
-//							}
-//						}
-//					}
-//				}
-//
-//				/*
-//				 * For Student Information
-//				 */
-//				for (XWPFParagraph p : doc.getParagraphs()) {
-//					for (XWPFRun r : p.getRuns()) {
-//						String text = r.getText(0);
-//						if (text != null && text.contains("$name$")) {
-//							text = text.replace("$name$", student.getFirstName() + " " + student.getMiddleName() + " "
-//									+ student.getLastName());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$std$")) {
-//							text = text.replace("$std$", student.getClassIn());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$reg$")) {
-//							text = text.replace("$reg$", student.getRegisterNumber());
-//							r.setText(text, 0);
-//						}
-//						r.setText(text, 0);
-//					}
-//				}
-////			doc.write(new FileOutputStream(new File("c://createdocument.docx")));
-//				File file = new File("ResultTempsc1.docx");
-//				doc.write(new FileOutputStream(file));
-//				if (file.exists()) {
-//					String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-//					if (mimeType == null) {
-//						mimeType = "application/octet-stream";
-//					}
-//					response.setContentType(mimeType);
-//					response.setContentLength((int) file.length());
-//					response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
-//							"attachment; filename=\"" + registerNumber + "_" + new Date().toString() + ".docx" + "\"");
-//					InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-//					FileCopyUtils.copy(inputStream, response.getOutputStream());
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		} else {
-//			response.sendRedirect("/exception");
-//		}
-//	}
+	@PostMapping("/school1/result/check/{registerNumber}")
+	public @ResponseBody BaseResponse check(@PathVariable(value = "registerNumber") String registerNumber) {
+		BaseResponse baseResponse = new BaseResponse();
+		School1_StudentEntity student = studentRepository.findByRegisterNumber(registerNumber);
+		List<School1_MarksEntity> marksList = marksRepository.findByRegisterNumber(registerNumber);
+		if (student == null) {
+			baseResponse.setStatus(300);
+			baseResponse.setType(ResponseType.RESPONSE_ERROR);
+			baseResponse.setMessage("Student Not Exits");
+			return baseResponse;
+		}
+		if (marksList.isEmpty()) {
+			baseResponse.setStatus(300);
+			baseResponse.setType(ResponseType.RESPONSE_ERROR);
+			baseResponse.setMessage("Marks Not Added");
+			return baseResponse;
+		}
 
-	@GetMapping("/school1/result/{registerNumber}")
+		baseResponse.setStatus(200);
+		return baseResponse;
+	}
+
+	@GetMapping("/school1/result/get/{registerNumber}")
 	public void getBonafideCertificate(@PathVariable(value = "registerNumber") String registerNumber,
 			HttpServletResponse response, HttpServletRequest request)
 			throws IOException, DocumentException, ParseException {
--- src/main/java/com/as/controller/school2/School2_ResultController.java
+++ src/main/java/com/as/controller/school2/School2_ResultController.java
@@ -24,7 +24,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.as.config.BaseResponse;
+import com.as.config.ResponseType;
 import com.as.entity.school2.School2_MarksEntity;
 import com.as.entity.school2.School2_StudentEntity;
 import com.as.repository.school2.School2_MarksRepository;
@@ -47,206 +51,29 @@ public class School2_ResultController {
 	@Autowired
 	School2_MarksRepository marksRepository;
 
-//	@GetMapping("/school2/result/{registerNumber}")
-//	public void getResult8_9_10(@PathVariable(value = "registerNumber") String registerNumber,
-//			HttpServletResponse response, HttpServletRequest request)
-//			throws IOException, DocumentException, ParseException {
-//
-//		School2_StudentEntity student = studentRepository.findByRegisterNumber(registerNumber);
-//		if (student != null) {
-//			List<School2_MarksEntity> marksList = marksRepository.findByRegisterNumber(registerNumber);
-//			try {
-//				ClassPathResource cpr = new ClassPathResource(env.getProperty("school2.result.doc.filePath"));
-//				XWPFDocument doc = new XWPFDocument(cpr.getInputStream());
-//				/*
-//				 * For Table
-//				 */
-//				for (XWPFTable tbl : doc.getTables()) {
-//					for (XWPFTableRow row : tbl.getRows()) {
-//						for (XWPFTableCell cell : row.getTableCells()) {
-//							for (XWPFParagraph p : cell.getParagraphs()) {
-//								for (XWPFRun r : p.getRuns()) {
-//									String text = r.getText(0).trim();
-//									/*
-//									 * For Marks Table
-//									 */
-//									for (School2_MarksEntity mark : marksList) {
-//										if (mark.getSubject().equalsIgnoreCase("qualitySkills")) {
-//											if (text != null && text.contains("|a1|")) {
-//												text = text.replace("|a1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|a2|")) {
-//												text = text.replace("|a2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("generalknowledge")) {
-//											if (text != null && text.contains("|b1|")) {
-//												text = text.replace("|b1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|b2|")) {
-//												text = text.replace("|b2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("language")) {
-//											if (text != null && text.contains("|c1|")) {
-//												text = text.replace("|c1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|c2|")) {
-//												text = text.replace("|c2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("mathematics")) {
-//											if (text != null && text.contains("|d1|")) {
-//												text = text.replace("|d1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|d2|")) {
-//												text = text.replace("|d2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("environmentScience")) {
-//											if (text != null && text.contains("|e1|")) {
-//												text = text.replace("|e1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|e2|")) {
-//												text = text.replace("|e2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("drawing")) {
-//											if (text != null && text.contains("|f1|")) {
-//												text = text.replace("|f1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|f2|")) {
-//												text = text.replace("|f2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//										if (mark.getSubject().equalsIgnoreCase("physicalEducation")) {
-//											if (text != null && text.contains("|g1|")) {
-//												text = text.replace("|g1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|g2|")) {
-//												text = text.replace("|g2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//
-//										if (mark.getSubject().equalsIgnoreCase("music")) {
-//											if (text != null && text.contains("|h1|")) {
-//												text = text.replace("|h1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|h2|")) {
-//												text = text.replace("|h2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//
-//										if (mark.getSubject().equalsIgnoreCase("total")) {
-//											if (text != null && text.contains("|i1|")) {
-//												text = text.replace("|i1|", mark.getGrade());
-//												r.setText(text, 0);
-//											}
-//											if (text != null && text.contains("|i2|")) {
-//												text = text.replace("|i2|", mark.getNote());
-//												r.setText(text, 0);
-//											}
-//										}
-//
-//									}
-//								}
-//							}
-//						}
-//					}
-//				}
-//
-//				/*
-//				 * For Student Information
-//				 */
-//				for (XWPFParagraph p : doc.getParagraphs()) {
-//					for (XWPFRun r : p.getRuns()) {
-//						String text = r.getText(0);
-//						if (text != null && text.contains("$name$")) {
-//							text = text.replace("$name$", student.getFirstName() + " " + student.getMiddleName() + " "
-//									+ student.getLastName());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$roll$")) {
-//							text = text.replace("$roll$", student.getRegisterNumber());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$std$")) {
-//							text = text.replace("$std$", student.getClassIn());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$age$")) {
-//							text = text.replace("$age$", student.getAge());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$dob$")) {
-//							text = text.replace("$dob$", student.getDateOfBirth());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$id$")) {
-//							text = text.replace("$id$", student.getDateInserted());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$ed$")) {
-//							text = text.replace("$ed$", student.getEvaluationdate());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$ht$")) {
-//							text = text.replace("$ht$", student.getHeight());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("$wt$")) {
-//							text = text.replace("$wt$", student.getWeight());
-//							r.setText(text, 0);
-//						}
-//						if (text != null && text.contains("|iq|")) {
-//							text = text.replace("|iq|", student.getIQ());
-//							r.setText(text, 0);
-//						}
-//
-//						r.setText(text, 0);
-//					}
-//				}
-////			doc.write(new FileOutputStream(new File("c://createdocument.docx")));
-//				File file = new File("ResultSc2Temp.docx");
-//				doc.write(new FileOutputStream(file));
-//				if (file.exists()) {
-//					String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-//					if (mimeType == null) {
-//						mimeType = "application/octet-stream";
-//					}
-//					response.setContentType(mimeType);
-//					response.setContentLength((int) file.length());
-//					response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
-//							"attachment; filename=\"" + registerNumber + "_" + new Date().toString() + ".docx" + "\"");
-//					InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-//					FileCopyUtils.copy(inputStream, response.getOutputStream());
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//
-//		} else {
-//			response.sendRedirect("/exception");
-//		}
-//	}
+	 @PostMapping("/school2/result/check/{registerNumber}")
+		public @ResponseBody BaseResponse check(@PathVariable(value = "registerNumber") String registerNumber) {
+			BaseResponse baseResponse=new BaseResponse();
+			School2_StudentEntity student = studentRepository.findByRegisterNumber(registerNumber);
+			List<School2_MarksEntity> marksList = marksRepository.findByRegisterNumber(registerNumber);
+			if (student == null) {
+				baseResponse.setStatus(300);
+				baseResponse.setType(ResponseType.RESPONSE_ERROR);
+				baseResponse.setMessage("Student Not Exits");
+				return baseResponse;
+			}
+			if(marksList.isEmpty()) {
+				baseResponse.setStatus(300);
+				baseResponse.setType(ResponseType.RESPONSE_ERROR);
+				baseResponse.setMessage("Marks Not Added");
+				return baseResponse;
+			}
+			
+			baseResponse.setStatus(200);
+			return baseResponse;
+		}
 
-	@GetMapping("/school2/result/{registerNumber}")
+	@GetMapping("/school2/result/get/{registerNumber}")
 	public void getResult8_9_10(@PathVariable(value = "registerNumber") String registerNumber,
 			HttpServletResponse response, HttpServletRequest request)
 			throws IOException, DocumentException, ParseException {
